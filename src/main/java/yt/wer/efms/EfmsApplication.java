60e855ea2ad16cc879657d30df61cc688b53e4ed
arnaud <arnaud.wery@student.uclouvain.be>
2025-10-25 16:05:30 +0200

    Added first site generation

---
+++ src/main/java/yt/wer/efms/EfmsApplication.java
@@ -0,0 +1,13 @@
+package yt.wer.efms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yt/wer/efms/EfmsApplicationTests.java
@@ -0,0 +1,13 @@
+package yt.wer.efms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
